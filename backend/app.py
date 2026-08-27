7e093c34f8ba10dcaaf7c4b8bfd71150f85b960b
igna-gnzlz <gonzalezignacio0404@gmail.com>
2026-08-27 20:48:41 -0300

    feat(backend): back rápido con FastAPI, tiene los requerimientos y eso

---
+++ backend/app.py
@@ -0,0 +1,53 @@
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, UploadFile, File
+import whisperx
+import tempfile
+import os
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:5500", "http://localhost:5500"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+model = whisperx.load_model(
+    "tiny",
+    device="cpu",
+    compute_type="int8",
+    language="es"
+)
+
+
+@app.get("/")
+def root():
+    return {"status": "ok"}
+
+
+@app.post("/transcribe")
+async def transcribe(file: UploadFile = File(...)):
+
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".webm") as temp:
+        content = await file.read()
+        temp.write(content)
+        temp_path = temp.name
+
+    try:
+        audio = whisperx.load_audio(temp_path)
+
+        result = model.transcribe(audio)
+
+        text = " ".join(
+            segment["text"]
+            for segment in result["segments"]
+        )
+
+        return {
+            "text": text
+        }
+
+    finally:
+        os.remove(temp_path)
